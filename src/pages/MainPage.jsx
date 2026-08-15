1c79e402db8e77bec6a01a6594456c26a207e6f5
dmitryi-88 <egorov.d.s83@mail.ru>
2026-08-15 21:41:17 +0300

    finished styling the main page

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -41,8 +41,59 @@ function MainPage() {
                     </div>
 
                     <div className={styles.description}>
-                        <span>{book.title}</span>
-                        <span>{book.author}</span>
+                        <span className={styles.title}>"{book.title}"</span>
+                        <hr />
+                        <span className={styles.author}>{book.author}</span>
+                    </div>
+
+                    <div className={styles.cardFooter}>
+                        {book.stock ? (
+                            <p>Осталось в наличии: {book.stock}</p>
+                        ) : (
+                            <p>Нет в наличии!</p>
+                        )}
+
+                        <hr />
+
+                        <div className={styles.priceAndCart}>
+                            <span>{book.price}₽</span>
+
+                            <button disabled={book.stock ? true : false}>
+                                <svg
+                                    width="28"
+                                    height="28"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        d="M3 5H5L7.2 16.2C7.4 17.2 8.3 18 9.3 18H17.5C18.5 18 19.4 17.3 19.7 16.3L21 10H6"
+                                        stroke="currentColor"
+                                        strokeWidth="1.9"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                    />
+                                    <circle
+                                        cx="9.5"
+                                        cy="21"
+                                        r="1.5"
+                                        fill="currentColor"
+                                    />
+                                    <circle
+                                        cx="17.5"
+                                        cy="21"
+                                        r="1.5"
+                                        fill="currentColor"
+                                    />
+                                    <path
+                                        d="M18 3V7M16 5H20"
+                                        stroke="currentColor"
+                                        strokeWidth="2"
+                                        strokeLinecap="round"
+                                    />
+                                </svg>
+                            </button>
+                        </div>
                     </div>
                 </div>
             ))}
